69108e3ec2a4bb03e793d85302853e285c3a509c
Deise Kinsk Reis Silva <deise.silva@livelo.com.br>
2022-03-29 12:52:59 -0300

    Extrai texto em array

--- index.js
+++ index.js
@@ -4,16 +4,18 @@
 import chalk from 'chalk';
 import * as fs from 'fs';
 
-const consulta = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
-
-
 
 function extractLinks(textFile){
     const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-    const result = textFile.match(regex);
-    console.log(result);
+    const result = [];
+    let cont;
+
+    while((cont = regex.exec(textFile)) !== null){
+        result.push({ [cont[1]]: cont[2]})
+    }
+
+    return result;
 }
-extractLinks(consulta);
 
 function findErro(erro){
     throw new Error(chalk.red(erro.code, 'Sem arquivo.'));
@@ -22,13 +24,14 @@ function findErro(erro){
 //função async
 async function getFileAsync(pathFile){
     try {
-    const textFile = await fs.promises
-    .readFile(pathFile,{encoding:'utf-8'})
-    console.log(chalk.green(textFile))
+    const textFile = await fs.promises.readFile(pathFile,{encoding:'utf-8'})
+    console.log(extractLinks(textFile))
+    
     } catch(erro){
         findErro(erro);
     } finally{
-    console.log(chalk.yellow('Operação finalizada'));
+    
+        console.log(chalk.yellow('Operação finalizada'));
     }
 }
 
