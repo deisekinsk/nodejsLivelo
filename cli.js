4a63c21352d125365516bca99bdaf7a6d537ecd2
Deise Kinsk Reis Silva <deise.silva@livelo.com.br>
2022-04-01 08:28:41 -0300

    Valida Links

--- cli.js
+++ cli.js
@@ -1,11 +1,18 @@
-import getFileAsync from   './index.js';
 import chalk from 'chalk';
+import getFileAsync from   './index.js';
+import checkURLs from './http-validacao.js';
+
 
 const path = process.argv;
 
 async function processFile(pathFile) {
     const result = await getFileAsync(pathFile[2]);
-    console.log(chalk.yellow('Lista de Links'), result)
+    if(path[3] === 'validate'){
+        console.log(chalk.green('Lista | Links Validados'), checkURLs(result));
+    }else{
+        console.log(chalk.yellow('Lista | Links'), result);
+    }
+    
 }
 
 processFile(path);
---
+++ http-validacao.js
@@ -0,0 +1,7 @@
+export default function checkURLs(arrayLinks){
+    return arrayLinks
+}
+
+
+
+
